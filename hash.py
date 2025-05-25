77fcbb0a4a8aa6ad42019b265bc47081bd8318ce
sumanthd032 <sumanthd032@gmail.com>
2025-05-26 00:07:08 +0530

    All features and files implemented

--- hash.py
+++
@@ -1,5 +0,0 @@
-from flask_bcrypt import Bcrypt
-bcrypt = Bcrypt()
-password = 'admin123'  # Replace with your desired password
-hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-print(hashed_password)
