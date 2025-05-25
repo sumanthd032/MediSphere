6c9050be70f4e704ebe37849f48d4f11a0cf0bfb
sumanthd032 <sumanthd032@gmail.com>
2025-05-25 23:54:41 +0530

    Remove config.py from version control

--- config.py
+++
@@ -1,7 +0,0 @@
-class Config: 
-    SECRET_KEY = 'abcdefghijklmnopq323fffdf' 
-    MYSQL_HOST = 'localhost' 
-    MYSQL_USER = 'root' 
-    MYSQL_PASSWORD = 'Jaiho@123' 
-    MYSQL_DB = 'medisphere' 
-    MYSQL_CURSORCLASS = 'DictCursor'
